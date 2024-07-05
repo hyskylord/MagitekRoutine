fb8e0a4cf6ee5af5e98e839d3fef8b54f6474917
cheesegoldfish <32321566+cheesegoldfish@users.noreply.github.com>
2024-07-05 05:16:23 -0500

    Adjust precastCooldown to prevent clipping

--- Magitek/Logic/Pictomancer/Palette.cs
+++ Magitek/Logic/Pictomancer/Palette.cs
@@ -90,7 +90,7 @@ namespace Magitek.Logic.Pictomancer
             else
             {
                 var castTime = motif.AdjustedCastTime.TotalMilliseconds;
-                var precastCooldown = (castTime + Globals.AnimationLockMs + BaseSettings.Instance.UserLatencyOffset) / muse.AdjustedCooldown.TotalMilliseconds;
+                var precastCooldown = (castTime + Globals.AnimationLockMs + BaseSettings.Instance.UserLatencyOffset) / muse.AdjustedCooldown.TotalMilliseconds + 500;
                 var precastThreshold = 1 - precastCooldown;
                 if (muse.Charges < precastThreshold) return false;
             }
