f48678cbc49508d5aa40adc4dcd404e4c9135573
cheesegoldfish <32321566+cheesegoldfish@users.noreply.github.com>
2024-08-02 23:29:34 -0500

    include assembly info in the build.

--- Magitek/Properties/AssemblyInfo.cs
+++ Magitek/Properties/AssemblyInfo.cs
@@ -1,4 +1,21 @@
 ﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Magitek")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Magitek")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("611a0cce-7786-430d-b866-f374ff03b289")]
 
 [assembly: AssemblyVersion("1.0.0.1")]
 [assembly: AssemblyFileVersion("1.0.0.1")]
