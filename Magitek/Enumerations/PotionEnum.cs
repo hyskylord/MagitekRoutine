046147c497cdd5a06363e14f74cee1dcc7041ad8
cheesegoldfish <32321566+cheesegoldfish@users.noreply.github.com>
2024-08-06 08:56:09 -0500

    Added Gemdraught Potions (INT/DEX/MIND/STR).

--- Magitek/Enumerations/PotionEnum.cs
+++ Magitek/Enumerations/PotionEnum.cs
@@ -4,21 +4,29 @@
     {
         None = 0,
 
+        Dex_Grade_10 = 44163,
+        Dex_Grade_9 = 44158,
         Dex_Grade_8 = 39728, 
         Dex_Grade_7 = 37841,
         Dex_Grade_6 = 36110,
         Dex_Grade_5 = 36105,
 
+        Intel_Grade_10 = 44165,
+        Intel_Grade_9 = 44160,
         Intel_Grade_8 = 39730, 
         Intel_Grade_7 = 37843,
         Intel_Grade_6 = 36112,
         Intel_Grade_5 = 36107,
 
+        Mind_Grade_10 = 44166,
+        Mind_Grade_9 = 44161,
         Mind_Grade_8 = 39731, 
         Mind_Grade_7 = 37844,
         Mind_Grade_6 = 36113,
         Mind_Grade_5 = 36108,
 
+        Strength_Grade_10 = 44162,
+        Strength_Grade_9 = 44157,
         Strength_Grade_8 = 39727, 
         Strength_Grade_7 = 37840,
         Strength_Grade_6 = 36109,
